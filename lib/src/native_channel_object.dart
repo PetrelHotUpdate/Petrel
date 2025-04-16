ccc646baea6f4839d4038395785434ee3cbfb0cb
josercc <josercc@163.com>
2025-04-16 10:57:38 +0800

    新增通道类型NativeChannelObjectList支持返回数组

--- lib/src/native_channel_object.dart
+++ lib/src/native_channel_object.dart
@@ -13,3 +13,20 @@ class DefaultNativeChannelObject<T> extends NativeChannelObject {
   @override
   Map<String, dynamic> toJson() => {'value': value};
 }
+
+class NativeChannelObjectList<T extends NativeChannelObject>
+    extends NativeChannelObject {
+  final List<T> value;
+  NativeChannelObjectList(this.value);
+
+  factory NativeChannelObjectList.fromJson(Map<String, dynamic> json,
+      T Function(Map<String, dynamic> json) fromJson) {
+    return NativeChannelObjectList<T>(
+        json['value'].map((e) => fromJson(e)).toList());
+  }
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'value': value.map((e) => e.toJson()).toList(),
+      };
+}
